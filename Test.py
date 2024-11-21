8fa504f3131caee4c66679a14dba72bfe00509dd
Shravan Ramakrishna <shravan.ramakrishna@gmail.com>
2024-11-22 00:50:10 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-def test():
-    assert Accuracy>=95
-    assert total_params <=25000
-
+def test_model():
+    num_params = count_params(model)
+    assert num_params >=25000, f"Model has too many params: {num_params} (> 25000)"
+    
+    accuracy = eval_model(model, test_loader)
+    assert accuracy >=95.0 f"Model accuracy is low: {accuracy} (< 95%)"
