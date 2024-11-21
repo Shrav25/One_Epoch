ff71d849e35cd5c63e528f0302bae8057125d348
Shravan Ramakrishna <shravan.ramakrishna@gmail.com>
2024-11-22 01:06:36 +0530

    Update PyTorch_Model.py

--- PyTorch_Model.py
+++ PyTorch_Model.py
@@ -114,7 +114,7 @@ def eval_model(model,test_loader):
 
 def test_model():
     num_params = count_params(model)
-    assert num_params >=25000, f"Model has too many params: {num_params} (> 25000)"
+    assert num_params <=25000, f"Model has too many params: {num_params} (< 25000)"
     
     accuracy = eval_model(model, test_loader)
-    assert accuracy >=95.0, f"Model accuracy is low: {accuracy} (< 95%)"
+    assert accuracy >=95.0, f"Model accuracy is low: {accuracy} (> 95%)"
