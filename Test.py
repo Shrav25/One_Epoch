e0893f344a9525bda0bf9d1a9db882310874f87d
Shravan Ramakrishna <shravan.ramakrishna@gmail.com>
2024-11-25 23:12:34 +0530

    Update Test.py
    Pakka to Pakka this will work

--- Test.py
+++ Test.py
@@ -4,15 +4,6 @@ import PyTorch_Model
 import torch
 
 
-@pytest.fixture
-def test_loader():
-    # You can define the DataLoader here for testing, similar to your main code
-    from torchvision import datasets, transforms
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5), (0.5))])
-    test_data = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
-    return test_loader
-
 def test_model_params():
     model = PyTorch_Model.MNISTModel()
     params_count = PyTorch_Model.count_params(model)
@@ -20,5 +11,6 @@ def test_model_params():
 
 def test_model_accuracy():
     model = PyTorch_Model.MNISTModel()
+    test_loader = test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=False)
     accuracy = PyTorch_Model.eval_model(model,test_loader)
     assert accuracy >=95, "Model accuracy is below 95%"
