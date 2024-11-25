b586fe304d823050fe8b57ff26a7eb4e9b2aa433
Shravan Ramakrishna <shravan.ramakrishna@gmail.com>
2024-11-25 23:04:32 +0530

    Update Test.py
    Pakka this will pass

--- Test.py
+++ Test.py
@@ -19,5 +19,6 @@ def test_model_params():
     assert params_count <=25000, "Model has more than 25K Params"
 
 def test_model_accuracy():
+    model = PyTorch_Model.MNISTModel()
     accuracy = PyTorch_Model.eval_model(model,test_loader)
     assert accuracy >=95, "Model accuracy is below 95%"
