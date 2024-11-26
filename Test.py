1a5723b1937d60c743d74d4eaca89498967c7d41
Shravan Ramakrishna <shravan.ramakrishna@gmail.com>
2024-11-26 16:48:52 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,7 +3,6 @@ import os
 import PyTorch_Model
 import torch
 
-
 def test_model_params():
     model = PyTorch_Model.MNISTModel()
     params_count = PyTorch_Model.count_params(model)
@@ -11,9 +10,6 @@ def test_model_params():
 
 def test_model_accuracy():
     model = PyTorch_Model.MNISTModel()
-    from torchvision import datasets, transforms
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5), (0.5))])
-    test_data = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True)
+    test_loader = PyTorch_Model.test_loader
     accuracy = PyTorch_Model.eval_model(model,test_loader)
     assert accuracy >=95, "Model accuracy is below 95%"
